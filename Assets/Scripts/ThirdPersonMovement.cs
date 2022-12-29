62a8a624ddde18af964ca6d9e3052212fddd5dd5
Augusta Steven Benedict <steven02asb@gmail.com>
2022-12-29 22:30:14 +0700

    awkward jump

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+
+    [Header("Movement")] 
+    private Vector3 moveDir;
     public Transform cam;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    private float turnSmoothVelocity;
+
+    [Header("Jump")] 
+    public float jumpSpeed;
+
+    public float gravity;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        
     }
 
     void Update()
@@ -21,16 +29,32 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(direction.magnitude >= 0.1f)
+        
+        if(controller.isGrounded)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); 
+                transform.rotation = Quaternion.Euler(0f, angle, 0f); 
+                moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                if(Input.GetButtonDown("Jump"))
+                {
+                    Debug.Log("can jump");
+                    moveDir.y = jumpSpeed;
+                    Debug.Log(moveDir);
+                }
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("can jump");
+                moveDir.y = jumpSpeed;
+                Debug.Log(moveDir);
+            }
         }
+        
+        moveDir.y += gravity * Time.deltaTime;
+        controller.Move(moveDir.normalized * (speed * Time.deltaTime));
     }
 
     private void OnApplicationFocus(bool focusStatus) 
