8ecfb4e1b500f2700c6e8e4f2175f1ef3afa047c
M T Y P <92069013+Prakasa2021@users.noreply.github.com>
2022-12-30 23:46:59 +0700

    fix awkward jump

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
 
     [Header("Movement")] 
     private Vector3 moveDir;
-    public Transform cam;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
     [Header("Jump")] 
-    public float jumpSpeed;
-
-    public float gravity;
+    public float gravity = -20f;
+    public float jumpHeight = 3;
+    Vector3 velocity;
+    bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     void Start()
     {
@@ -25,36 +29,35 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -4f;
+        }
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -1 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        
-        if(controller.isGrounded)
+
+        if(direction.magnitude >= 0.1f)
         {
-            if (direction.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); 
-                transform.rotation = Quaternion.Euler(0f, angle, 0f); 
-                moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                if(Input.GetButtonDown("Jump"))
-                {
-                    Debug.Log("can jump");
-                    moveDir.y = jumpSpeed;
-                    Debug.Log(moveDir);
-                }
-            }
-            if (Input.GetButtonDown("Jump"))
-            {
-                Debug.Log("can jump");
-                moveDir.y = jumpSpeed;
-                Debug.Log(moveDir);
-            }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        
-        moveDir.y += gravity * Time.deltaTime;
-        controller.Move(moveDir.normalized * (speed * Time.deltaTime));
     }
 
     private void OnApplicationFocus(bool focusStatus) 
