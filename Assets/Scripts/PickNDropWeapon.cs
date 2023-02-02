e35065a182df6f51e1022a52463fd8d0ee33a1f2
M T Y P <92069013+Prakasa2021@users.noreply.github.com>
2023-02-02 17:33:17 +0700

    Weapon pickup (FIX)

--- Assets/Scripts/PickNDropWeapon.cs
+++ Assets/Scripts/PickNDropWeapon.cs
@@ -4,57 +4,82 @@ using UnityEngine;
 
 public class PickNDropWeapon : MonoBehaviour
 {
-    public GameObject handler;
-    float maxpickupdistance = 5;
-    GameObject itemcurrentlyholding;
-    bool isholding = false;
+    // public ProjectileGun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, tppCam;
 
-    void Update()
+    public float pickUpRange, pickUpTime;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if(!equipped)
         {
-            Pickup();
+            // gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
         }
-        if (Input.GetKeyDown(KeyCode.G))
+
+        if(equipped)
+        {
+            // gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if(!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull)
+        {
+            PickUp();
+        }
+
+        if(equipped && Input.GetKeyDown(KeyCode.F))
         {
             Drop();
         }
     }
 
-    public void Pickup()
+    private void PickUp()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(handler.transform.position, handler.transform.forward, out hit, maxpickupdistance))
-        {
-            if(hit.transform.tag == "Weapon")
-            {
-                if (isholding) Drop();
+        equipped = true;
+        slotFull = true;
 
-                itemcurrentlyholding = hit.transform.gameObject;
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
 
-                foreach (var c in hit.transform.GetComponentsInChildren<Collider>()) if (c != null) c.enabled = false;
-                foreach (var r in hit.transform.GetComponentsInChildren<Rigidbody>()) if (r != null) r.isKinematic = true;
+        rb.isKinematic = true;
+        coll.isTrigger = true;
 
-                itemcurrentlyholding.transform.parent = transform;
-                itemcurrentlyholding.transform.localPosition = Vector3.zero;
-                itemcurrentlyholding.transform.localEulerAngles = Vector3.zero;
-
-                isholding = true;
-                Debug.Log("Pickup");
-            }
-        }
+        // gunScript.enabled = true;
     }
 
-    public void Drop()
+    private void Drop()
     {
-        itemcurrentlyholding.transform.parent = null;
-        foreach (var c in itemcurrentlyholding.GetComponentsInChildren<Collider>()) if (c != null) c.enabled = true;
-        foreach (var r in itemcurrentlyholding.GetComponentsInChildren<Rigidbody>()) if (r != null) r.isKinematic = false;
-        isholding = false;
-        RaycastHit hitDown;
-        Physics.Raycast(transform.position, -Vector3.up, out hitDown);
+        equipped = false;
+        slotFull = false;
 
-        itemcurrentlyholding.transform.position = hitDown.point + new Vector3(transform.forward.x, 0, transform.forward.z);
-        itemcurrentlyholding = null; 
+        transform.SetParent(null);
+
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        // rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        rb.AddForce(tppCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(tppCam.up * dropUpwardForce, ForceMode.Impulse);
+
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random) * 10);
+
+        // gunScript.enabled = false;
     }
 }
