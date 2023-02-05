2683827fd63949ab2df68c8f4cfde4f909a46540
M T Y P <92069013+Prakasa2021@users.noreply.github.com>
2023-02-05 17:15:35 +0700

    Box pickup & fix some bugs

--- Assets/Scripts/PickNDropBox.cs
+++ Assets/Scripts/PickNDropBox.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickNDropWeapon : MonoBehaviour
+public class PickNDropBox : MonoBehaviour
 {
     // public ProjectileGun gunScript;
     public Rigidbody rb;
     public BoxCollider coll;
-    public Transform player, gunContainer, tppCam;
+    public Transform player, boxContainer, tppCam;
 
     public float pickUpRange;
     public float dropForwardForce, dropUpwardForce;
@@ -19,14 +19,12 @@ public class PickNDropWeapon : MonoBehaviour
     {
         if(!equipped)
         {
-            // gunScript.enabled = false;
             rb.isKinematic = false;
             coll.isTrigger = false;
         }
 
         if(equipped)
         {
-            // gunScript.enabled = true;
             rb.isKinematic = true;
             coll.isTrigger = true;
         }
@@ -51,7 +49,7 @@ public class PickNDropWeapon : MonoBehaviour
         equipped = true;
         slotFull = true;
 
-        transform.SetParent(gunContainer);
+        transform.SetParent(boxContainer);
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.Euler(Vector3.zero);
         transform.localScale = Vector3.one;
@@ -59,9 +57,7 @@ public class PickNDropWeapon : MonoBehaviour
         rb.isKinematic = true;
         coll.isTrigger = true;
 
-        GetComponent<BulletSpawn>().enabled = true;
-        GameObject.Find("BoxPrototype").GetComponent<PickNDropBox>().enabled = false;
-        // gunScript.enabled = true;
+        GameObject.Find("WeaponPrototype").GetComponent<PickNDropWeapon>().enabled = false;
     }
 
     private void Drop()
@@ -74,16 +70,12 @@ public class PickNDropWeapon : MonoBehaviour
         rb.isKinematic = false;
         coll.isTrigger = false;
 
-        // rb.velocity = player.GetComponent<Rigidbody>().velocity;
-
         rb.AddForce(tppCam.forward * dropForwardForce, ForceMode.Impulse);
         rb.AddForce(tppCam.up * dropUpwardForce, ForceMode.Impulse);
 
         float random = Random.Range(-1f, 1f);
         rb.AddTorque(new Vector3(random, random, random) * 10);
 
-        GetComponent<BulletSpawn>().enabled = false;
-        GameObject.Find("BoxPrototype").GetComponent<PickNDropBox>().enabled = true;
-        // gunScript.enabled = false;
+        GameObject.Find("WeaponPrototype").GetComponent<PickNDropWeapon>().enabled = true;
     }
 }
